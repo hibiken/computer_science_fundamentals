98b7a6ce31f50b32e4b490a497749c6ae912af06
Ken Hibino <ken.hibino7@gmail.com>
2017-08-12 10:12:59 -0700

    Add insertion sort

---
+++ algorithms/sort/insertion_sort.js
@@ -0,0 +1,16 @@
+const insertionSort = (nums) => {
+  for (let j = 1; j < nums.length; j++) {
+    let key = nums[j];
+    let i = j - 1; // nums[0..i] is sorted
+
+    while (i >= 0 && nums[i] > key) {
+      nums[i+1] = nums[i];
+      i = i - 1
+    }
+    nums[i+1] = key;
+  }
+
+  return nums;
+};
+
+console.log('result', insertionSort([2, 5, 3, 1, 6, 4]));
