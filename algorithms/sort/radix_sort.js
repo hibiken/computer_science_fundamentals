06ac7ddf3c38f442e1c315ba331c5d001947f82e
Ken Hibino <ken.hibino7@gmail.com>
2017-10-14 12:44:59 -0700

    Update radix sort implementation

--- algorithms/sort/radix_sort.js
+++ algorithms/sort/radix_sort.js
@@ -1,71 +1,54 @@
-/* Time complexity: O(N)
- * where N is the length of nums
- */
-const radixSort = (nums) => {
-  const max = Math.max(...nums);
-
-  const maxDigitCount = Math.floor(Math.log10(max)) + 1;
-
-  for (let i = 1; i <= maxDigitCount; i++) {
-    const sortBy = nums.map(num => getDigit(num, i));
-    nums = countingSort(sortBy, nums);
+const countingSort = (nums, k, getKey) => {
+  const counterArray = new Array(k + 1);
+  // O(k) time;
+  for (let i = 0; i < counterArray.length; i++) {
+    counterArray[i] = 0;
   }
 
-  return nums;
+  // O(N) time;
+  for (let i = 0; i < nums.length; i++) {
+    const key = getKey(nums[i]);
+    counterArray[key] = counterArray[key] + 1;
+  }
+
+  // O(k) time;
+  for (let i = 1; i < counterArray.length; i++) {
+    counterArray[i] = counterArray[i] + counterArray[i-1];
+  };
+
+  const output = new Array(nums.length);
+
+  // O(N) time;
+  for (let j = nums.length - 1; j >= 0; j--) {
+    const key = getKey(nums[j]);
+    const index = counterArray[key] - 1;
+    counterArray[key] = counterArray[key] - 1;
+    output[index] = nums[j];
+  }
+
+  return output;
 };
 
-const getDigit = (num, place) => {
-  return Math.floor(num % (Math.pow(10, place)) / Math.pow(10, place - 1));
-}
-
-const countingSort = (nums, numsToSort) => {
-  const min = Math.min(...nums); // O(N) time
-  const max = Math.max(...nums); // O(N) time
-  const numCount = new Array(max - min + 1); // O(k) time & space: where k in range of input (max - min + 1)
-  const indexFor = num => num - min;
-
-  // initialize numCount elements to zeros
-  // O(k) time
-  for (let i = 0; i < numCount.length; i++) {
-    numCount[i] = 0;
+const radixSort = (nums, d) => {
+  let sorted = nums;
+  for (let i = 1; i <= d; i++) {
+    const getKey = createDigitGetterFunc(i);
+    sorted = countingSort(sorted, 9, getKey);
   }
-
-  // O(N)
-  for (let i = 0; i < nums.length; i++) {
-    numCount[indexFor(nums[i])] += 1;
-  }
-
-  const less = new Array(numCount.length);
-  less[0] = 0;;
-  for (let j = 1; j < less.length; j++) {
-    less[j] = less[j-1] + numCount[j-1];
-  }
-  // O(N) time & space
-  const result = new Array(nums.length);
-
-  // O(N)
-  for (let i = 0; i < nums.length; i++) {
-    const num = nums[i];
-    const index = less[indexFor(num)];
-    less[indexFor(num)] += 1;
-    result[index] = numsToSort[i];
-  }
-
-  return result;
+  return sorted;
 };
 
-/* Analysis 
- *
- * Time Complexity: O(N + k):
- * where N is the size of the input, k is range of
- * input 
- *
- * Auxilary Space: O(N + k)
- */
+// Return a function to get i-th digit of the input integer
+const createDigitGetterFunc = (i) => {
+  return (integer) => {
+    if (integer < Math.pow(10, i - 1)) return 0;
+    const remainder = integer % (Math.pow(10, i));
+    return Math.floor(remainder / (Math.pow(10, i - 1)));
+  };
+};
 
 
-const numbers = [61, 74, 9, 513, 59, 2, 321, 433];
-const smallSample = [1, -4, 2, 3, 5, -2];
-// console.log(countingSort([1, 4, 1, 9, 2, 3], numbers));
-console.log('Result', radixSort(numbers));
+/* Test */
+const numbers = [821, 73453, 31345, 22, 10,  5000000, 1, 900, 8421, 290, 222828];
+console.log(radixSort(numbers, 7));
 
