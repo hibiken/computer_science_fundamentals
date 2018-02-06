52bc8b6fea77c679ffef4c7895139a958895b4c9
Ken Hibino <ken.hibino7@gmail.com>
2018-02-06 07:19:24 -0800

    Add heap sort implementation in Python

--- Python/algorithms/sort_and_search/heap_sort.py
+++ Python/algorithms/sort_and_search/heap_sort.py
@@ -1,8 +1,6 @@
-# Implementation of Priority Queue ADT
-# Min Heap
-class MinHeap:
+class MaxHeap(object):
     def __init__(self):
-        self.heapList = [0] # fill zero index for easier integer division for indexing.
+        self.heapList = [0]
         self.currentSize = 0
 
     def insert(self, k):
@@ -12,13 +10,13 @@ class MinHeap:
 
     def heapifyUp(self, i):
         while i // 2 > 0:
-            if self.heapList[i] < self.heapList[i // 2]:
+            if self.heapList[i] > self.heapList[i // 2]:
                 temp = self.heapList[i // 2]
                 self.heapList[i // 2] = self.heapList[i]
                 self.heapList[i] = temp
             i = i // 2
 
-    def extractMin(self):
+    def extractMax(self):
         retval = self.heapList[1]
         self.heapList[1] = self.heapList[self.currentSize]
         self.currentSize -= 1
@@ -28,18 +26,18 @@ class MinHeap:
 
     def heapifyDown(self, i):
         while (i * 2) <= self.currentSize:
-            mc = self.minChild(i)
-            if self.heapList[i] > self.heapList[mc]:
+            mc = self.maxChild(i)
+            if self.heapList[i] < self.heapList[mc]:
                 temp = self.heapList[i]
                 self.heapList[i] = self.heapList[mc]
                 self.heapList[mc] = temp
             i = mc
 
-    def minChild(self, i):
+    def maxChild(self, i):
         if i * 2 + 1 > self.currentSize:
             return i * 2
         else:
-            if self.heapList[i*2] < self.heapList[i*2+1]:
+            if self.heapList[i*2] > self.heapList[i*2+1]:
                 return i * 2
             else:
                 return i * 2 + 1
@@ -51,3 +49,16 @@ class MinHeap:
         while i > 0:
             self.heapifyDown(i)
             i -= 1
+
+    def isEmpty(self):
+        return self.currentSize == 0
+
+
+if __name__ == "__main__":
+    sample = [32, 4, 24, 56, 21, 10, 9, 67, 20, 18, 1, 0]
+    heap = MaxHeap()
+    heap.buildHeap(sample)
+    ordered = []
+    while not heap.isEmpty():
+        ordered.append(heap.extractMax())
+    print(ordered)
